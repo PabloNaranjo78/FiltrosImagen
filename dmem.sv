aa57de317bd9fc992a37068b763e753d95d2e1b6
andyrrr <43052958+andyrrr@users.noreply.github.com>
2023-11-22 06:33:55 -0600

    end modules

--- dmem.sv
+++ dmem.sv
@@ -4,10 +4,13 @@ module dmem(
 	output logic [31:0] rd
 );
 
-
-
 	logic [31:0] RAM[63:0];
+	
+	
 	assign rd = RAM[a[31:2]]; // word aligned
+	
 	always_ff @(posedge clk)
 		if (we) RAM[a[31:2]] <= wd;
+
+		
 endmodule
--- imem.sv
+++ imem.sv
@@ -3,10 +3,10 @@ module imem(
 	output logic [31:0] rd
 );
 	
-	logic [31:0] RAM[63:0];
+	logic [31:0] ROM[63:0];
 	
 	
 	initial
-		$readmemh("memfile.dat",RAM);
-		assign rd = RAM[a[31:2]]; // word aligned
+		$readmemh("memfile.dat",ROM);
+		assign rd = ROM[a[31:2]]; // word aligned
 endmodule
