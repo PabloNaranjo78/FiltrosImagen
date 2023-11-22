ce3e84797b301edcc1d0532a9724bb54ae2a2ec2
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2023-11-22 03:26:15 -0600

    restador

--- N_substractor_module.sv
+++ N_substractor_module.sv
@@ -1,52 +1,56 @@
-module N_substractor_module #(parameter int N = 32)(
-    input logic [N-1:0] a,
-    input logic [N-1:0] b,
-    output logic [N-1:0] z,
-    output logic cout,
+module N_substractor_module #(parameter int n = 32)(
+    input logic [n-1:0] A,
+    input logic [n-1:0] B,
+    output logic [n-1:0] z,
+    output logic Cout,
     output logic negative_flag,
-    output logic zero_flag
+    output logic zero_flag,
+    output logic overflow_flag
 );
 
-    reg [N:0] carryaux = 0;
-    logic flag = 0;
-    reg [N-1:0] comp = 0;
-
+    reg [n:0] carry_out_intermediate=0;
+	 logic flag = 0;
+	 reg [n-1:0] complemento =0;
+    
+    // Instancias de sumadores de 1 bit
     genvar i;
     generate
-        for (i = 0; i < N; i = i + 1) begin : loop_01
-            restador res(
-                .a(a[i]),
-                .b(b[i]),
-                .cin(carryaux[i]),
-                .z(comp[i]),
-                .cout(carryaux[i+1])
-            );
-        end
-        assign cout = carryaux[N];
+		for (i = 0; i < n; i = i + 1) begin: loop_01
+			substractor_module res(
+				.a(A[i]),
+				.b(B[i]),
+				.cin(carry_out_intermediate[i]),
+				.z(complemento[i]),
+            .cout(carry_out_intermediate[i+1])
+			);
+		end
+		assign Cout = carry_out_intermediate[n];
     endgenerate
+	 
+	 always @(*) begin
+		if (Cout ==1) begin
+			flag= 0;
+			for (int i = 0; i < n; i = i + 1) begin
+				if (flag) begin 
+					z[i]= ~complemento[i];
+				end else begin 
+					z[i]= complemento[i];
+					if (z[i]==1 && flag == 0) begin 
+						flag = 1;
+					end
+				end
+			end
+			$display("Num: %b Compl: %b", z, complemento);
+		end else begin
+			for (int i = 0; i < n; i = i + 1) begin
+				z[i]= complemento[i];
+			end
+		end
+	end
 
-    always_comb begin
-        if (cout == 1) begin
-            flag = 0;
-            for (int i = 0; i < N; i = i + 1) begin
-                if (flag) begin 
-                    z[i] = ~comp[i];
-                end else begin 
-                    z[i] = comp[i];
-                    if (z[i] == 1 && flag == 0) begin 
-                        flag = 1;
-                    end
-                end
-            end
-        end else begin
-            for (int i = 0; i < N; i = i + 1) begin
-                z[i] = comp[i];
-            end
-        end
+    assign negative_flag = Cout;
+    assign zero_flag = (z == 0);
+    assign overflow_flag = (A[n-1] == 1 && B[n-1] == 0 && z[n-1] == 0) || (A[n-1] == 0 && B[n-1] == 1 && z[n-1] == 1);
 
-        // Flags
-        negative_flag = flag;
-        zero_flag = (&z == 0);
-    end
 
 endmodule
--- test_bench.sv
+++ test_bench.sv
@@ -1,6 +1,6 @@
 module test_bench();
-
-    reg [3:0] A_input;
+/*
+    reg [3:0] A;
     reg [3:0] B_input;
     wire [3:0] Sum_output;
     wire carry_flag, overflow_flag, zero_flag;
@@ -52,5 +52,109 @@ module test_bench();
 		  
 		  
     end
+*/
+
+ // Declaración de señales de prueba
+  logic [3:0] A;
+  logic [3:0] B;
+  logic Cin;
+  logic [3:0] resta;
+  logic Cout, zero_flag, negative_flag;
+  
+  // Instancia del módulo bajo prueba
+  N_substractor_module uut (
+    .A(A),
+    .B(B),
+    .z(resta),
+    .Cout(Cout),
+    .negative_flag(negative_flag),
+    .zero_flag(zero_flag)
+  );
+
+  // Inicialización de señales
+  initial begin
+
+
+
+        // Prueba 1
+        A = 4'b0101;
+        B = 4'b1100;
+        #40;
+		  
+
+		  
+        // Prueba 1
+        A = 4'b1111;
+        B = 4'b1111;
+        #40;
+		  
+
+
+        // Prueba 1
+        A = 4'b1010;
+        B = 4'b1111;
+        #40;
+		  
+
+
+        // Prueba 1
+        A = 4'b0001;
+        B = 4'b0010;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0001;
+        B = 4'b0011;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0001;
+        B = 4'b0100;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0001;
+        B = 4'b0110;
+        #40;
+		  // Prueba 1
+        A = 4'b0001;
+        B = 4'b0111;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0010;
+        B = 4'b0011;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0010;
+        B = 4'b0100;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0010;
+        B = 4'b0110;
+        #40;
+		  // Prueba 1
+        A = 4'b0010;
+        B = 4'b0111;
+        #40;
+
+		  
+		  // Prueba 1
+        A = 4'b0011;
+        B = 4'b0100;
+        #40;
+		  
+		  // Prueba 1
+        A = 4'b0011;
+        B = 4'b0110;
+        #40;
+		  // Prueba 1
+        A = 4'b0011;
+        B = 4'b0111;
+        #40;
+    
+  end
 
 endmodule 
