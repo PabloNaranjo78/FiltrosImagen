66ad6abf397914166e1d54cc9cdf97f7926cbd6e
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2023-11-22 00:04:26 -0600

    Sumador completado

--- N_fulladder_module.sv
+++ N_fulladder_module.sv
@@ -2,7 +2,6 @@ module N_fulladder_module #(parameter n=32) (
     input logic [n-1:0] A,
     input logic [n-1:0] B,
     output logic [n-1:0] Sum,
-    output logic Cout,
     output carry_flag,
     output overflow_flag,
     output zero_flag
@@ -26,36 +25,14 @@ reg [n:0] carry_out_intermediate=0;
         end
     endgenerate
 	
-	assign Cout = carry_out_intermediate[n];
+	
+	// Flags
+	assign carry_flag = carry_out_intermediate[n];
+  assign overflow_flag = carry_out_intermediate[n];
+	assign zero_flag = (Sum == 0) & carry_flag == 0;
+	
 
-
-    // Flags
-   // assign carry_flag = cout_intermediate[N-1];
-   // assign overflow_flag = |(sum_intermediate > (2**N-1));
-    //assign zero_flag = (&sum == 0);
 
 endmodule
 
-module nsumador #(parameter N = 4) (
-  input logic [N-1:0] a,
-  input logic [N-1:0] b,
-  output logic [N-1:0] result,
-  output logic carryOut
-  
-);
-
-  wire [N:0] carries;
-  genvar i;
-
-  generate 
-    for (i = 0; i < N; i = i + 1)
-     begin: nadder
-      sumador s1 (.a(a[i]), .b(b[i]), .cin(carries[i]), .z(result[i]), .cout(carries[i+1]));
-        end
-    endgenerate
-//	 assign result[N] = carries[N];
-	 assign carryOut = carries[N];
-	 
-
-endmodule
 
--- test_bench.sv
+++ test_bench.sv
@@ -3,20 +3,23 @@ module test_bench();
     reg [3:0] A_input;
     reg [3:0] B_input;
     wire [3:0] Sum_output;
-    wire Cout_output;
+    wire carry_flag, overflow_flag, zero_flag;
 
     // Instanciar el módulo SumaNBits
     N_fulladder_module #(4) dut (
         .A(A_input),
         .B(B_input),
         .Sum(Sum_output),
-        .Cout(Cout_output)
+        .carry_flag(carry_flag),
+        .overflow_flag(overflow_flag),
+        .zero_flag(zero_flag)
+
     );
 
     // Generar estímulos
     initial begin
         $display("A\tB\tSum\tCout");
-        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, Cout_output);
+        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, carry_flag);
 
         // Prueba 1
         A_input = 4'b0101;
@@ -24,7 +27,7 @@ module test_bench();
         #40;
 		  
 		  $display("A\tB\tSum\tCout");
-        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, Cout_output);
+        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, carry_flag);
 		  
         // Prueba 1
         A_input = 4'b1111;
@@ -32,7 +35,7 @@ module test_bench();
         #40;
 		  
 		  $display("A\tB\tSum\tCout");
-        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, Cout_output);
+        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, carry_flag);
 
         // Prueba 1
         A_input = 4'b1010;
@@ -40,7 +43,7 @@ module test_bench();
         #40;
 		  
 		  $display("A\tB\tSum\tCout");
-        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, Cout_output);
+        $monitor("%b\t%b\t%b\t%b", A_input, B_input, Sum_output, carry_flag);
 
         // Prueba 1
         A_input = 4'b000;
